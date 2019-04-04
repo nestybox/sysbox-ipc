0bdd6116efdd811c40d0c7cdf397194cc4caf15c
ctv10 <ctalledo@gmail.com>
2019-04-04 16:56:54 -0700

    Created sysvisorGrpc package.

---
+++ sysvisorGrpc/grpcClient.go
@@ -0,0 +1,154 @@
+package sysvisorGrpc
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	pb "github.com/nestybox/sysvisor/sysvisor-protobuf"
+	"google.golang.org/grpc"
+)
+
+const sysvisor_address = "localhost:50052"
+
+// Container info passed by the client to the server across the grpc channel
+type ContainerData struct {
+	Id       string
+	InitPid  int32
+	Hostname string
+	Ctime    time.Time
+	UidFirst int32
+	UidSize  int32
+	GidFirst int32
+	GidSize  int32
+}
+
+//
+// Establishes grpc connection to sysvisor-fs' remote-end.
+//
+func sysvisorfs_connect() *grpc.ClientConn {
+
+	// Set up a connection to the server.
+	// TODO: Secure me through TLS.
+	conn, err := grpc.Dial(sysvisor_address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to Sysvisorfs: %v", err)
+		return nil
+	}
+
+	return conn
+}
+
+func containerDataToPbData(data *ContainerData) (*pb.ContainerData, error) {
+	pbTime, err := ptypes.TimestampProto(data.Ctime)
+	if err != nil {
+		return nil, fmt.Errorf("time conversion error: %v", err)
+	}
+
+	return &pb.ContainerData{
+		Id:      data.Id,
+		InitPid: data.InitPid,
+		Ctime:   pbTime,
+	}, nil
+}
+
+//
+// Registers container creation in sysvisor-fs. Notice that this
+// is a blocking call that can potentially have a minor impact
+// on container's boot-up speed.
+//
+func SendContainerRegistration(data *ContainerData) (err error) {
+	var pbData *pb.ContainerData
+
+	// Set up sysvisorfs pipeline.
+	conn := sysvisorfs_connect()
+	if conn == nil {
+		return fmt.Errorf("failed to connect with sysvisor-fs")
+	}
+	defer conn.Close()
+
+	cntrChanIntf := pb.NewContainerStateChannelClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	pbData, err = containerDataToPbData(data)
+	if err != nil {
+		return fmt.Errorf("convertion to protobuf data failed: %v", err)
+	}
+
+	_, err = cntrChanIntf.ContainerRegistration(ctx, pbData)
+	if err != nil {
+		return fmt.Errorf("failed to register container with sysvisor-fs: %v", err)
+	}
+
+	return nil
+}
+
+//
+// Unregisters container from Sysvisorfs.
+//
+func SendContainerUnregistration(data *ContainerData) (err error) {
+	var pbData *pb.ContainerData
+
+	// Set up sysvisorfs pipeline.
+	conn := sysvisorfs_connect()
+	if conn == nil {
+		return fmt.Errorf("failed to connect with sysvisor-fs")
+	}
+	defer conn.Close()
+
+	cntrChanIntf := pb.NewContainerStateChannelClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	pbData, err = containerDataToPbData(data)
+	if err != nil {
+		return fmt.Errorf("convertion to protobuf data failed: %v", err)
+	}
+
+	_, err = cntrChanIntf.ContainerUnregistration(ctx, pbData)
+	if err != nil {
+		return fmt.Errorf("failed to unregister container with sysvisor-fs: %v", err)
+	}
+
+	return nil
+}
+
+//
+// Sends a container-update message to sysvisor-fs end. At this point, we are
+// only utilizing this message for a particular case, update the container
+// creation-time attribute, but this function can serve more general purposes
+// in the future.
+//
+func SendContainerUpdate(data *ContainerData) (err error) {
+	var pbData *pb.ContainerData
+
+	// Set up sysvisorfs pipeline.
+	conn := sysvisorfs_connect()
+	if conn == nil {
+		return fmt.Errorf("failed to connect with sysvisor-fs")
+	}
+	defer conn.Close()
+
+	cntrChanIntf := pb.NewContainerStateChannelClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	pbData, err = containerDataToPbData(data)
+	if err != nil {
+		return fmt.Errorf("convertion to protobuf data failed: %v", err)
+	}
+
+	_, err = cntrChanIntf.ContainerUpdate(ctx, pbData)
+	if err != nil {
+		return fmt.Errorf("failed to send container-update message to ",
+			"sysvisor-fs: %v", err)
+	}
+
+	return nil
+}
