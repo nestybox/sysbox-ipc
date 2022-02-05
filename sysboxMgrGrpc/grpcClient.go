dac4be72c3ae50f042290e5cc860b0a4706aeae3
Cesar Talledo <ctalledo@nestybox.com>
2022-02-05 22:14:29 +0000

    Increase sysbox-mgr grpc timeout due to rootfs chown delays.
    For hosts without shiftfs, Sysbox performs chown of the rootfs.
    To do this quickly, the sysbox-mgr clones the rootfs and
    enables metacopy=on on the rootfs overlayfs mount.
    
    However, even with metacopy=on, the rootfs chown can take a
    good amount of time. And if several containers are started
    at the same time, the delay can add up causing the grpc
    channel between sysbox-runc and sysbox-mgr to time out.
    
    This commit fixes this by increasing the timeout from
    20 secs to 30 secs. Tests verify that this is sufficient.
    
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- sysboxMgrGrpc/grpcClient.go
+++ sysboxMgrGrpc/grpcClient.go
@@ -56,7 +56,7 @@ func Register(regInfo *ipcLib.RegistrationInfo) (*ipcLib.ContainerConfig, error)
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.RegisterReq{
@@ -95,7 +95,7 @@ func Update(updateInfo *ipcLib.UpdateInfo) error {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.UpdateReq{
@@ -123,7 +123,7 @@ func Unregister(id string) error {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.UnregisterReq{
@@ -147,7 +147,7 @@ func SubidAlloc(id string, size uint64) (uint32, uint32, error) {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.SubidAllocReq{
@@ -226,7 +226,7 @@ func PrepMounts(id string, uid, gid uint32, prepList []ipcLib.MountPrepInfo) err
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	// Convert []ipcLib.MountPrepInfo -> []*pb.MountPrepInfo
@@ -268,7 +268,7 @@ func ReqShiftfsMark(id string, mounts []configs.ShiftfsMount) ([]configs.Shiftfs
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	// convert configs.ShiftfsMount to grpc ShiftfsMark
@@ -315,7 +315,7 @@ func ReqFsState(id, rootfs string) ([]configs.FsEntry, error) {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.FsStateReq{
@@ -355,7 +355,7 @@ func Pause(id string) error {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.PauseReq{
@@ -417,7 +417,7 @@ func ReqCloneRootfs(id string) (string, error) {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.CloneRootfsReq{
@@ -442,7 +442,7 @@ func ChownClonedRootfs(id string, uidOffset, gidOffset int32) error {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.ChownClonedRootfsReq{
@@ -469,7 +469,7 @@ func RevertClonedRootfsChown(id string) error {
 	defer conn.Close()
 
 	ch := pb.NewSysboxMgrStateChannelClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	req := &pb.RevertClonedRootfsChownReq{
